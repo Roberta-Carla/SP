62788f0ba9f0abcff54ba0268a6aac69a0f88f1f
C_Roberta <robertacarla6111@gmail.com>
2020-10-27 12:32:04 +0200

    Lab4

---
+++ src/Lab_4/ImageProxy.java
@@ -0,0 +1,17 @@
+package Lab_4;
+
+public class ImageProxy implements Element{
+    private String url;
+    Image realImage;
+
+    ImageProxy(String url){
+        this.url=url;
+    }
+
+    public void print() {
+        if(realImage==null){
+            realImage = new Image(this.url);
+        }
+        realImage.print();
+    }
+}
---
+++ src/Lab_4/Paragraph.java
@@ -0,0 +1,15 @@
+package Lab_4;
+
+public class Paragraph implements Element{
+
+    private String name;
+
+    Paragraph(String name){
+        this.name=name;
+    }
+
+    public void print(){
+        System.out.println("Paragraph with name: "+this.name);
+    }
+
+}
